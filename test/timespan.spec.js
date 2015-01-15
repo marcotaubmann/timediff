b2be5b6a42c78943b359f0aca0236ca7aa6271c1
Marco Taubmann <marco.taubmann@gmail.com>
2015-01-15 11:20:12 +0100

    Timespan function with tests

---
+++ test/timespan.spec.js
@@ -0,0 +1,133 @@
+var timespan = require('../timespan.js');
+var moment = require('moment');
+
+describe('timespan', function () {
+
+  it('should accept only parsable dates as first two parameters', function () {
+    expect(function () {timespan('no date'  , new Date()     )}).toThrow();
+    expect(function () {timespan(new Date() , function () {} )}).toThrow();
+
+    expect(function () {timespan(new Date() , new Date())}).not.toThrow();
+    expect(function () {timespan('2015-01-12', new Date())}).not.toThrow();
+    expect(function () {timespan(new Date() , '2015-01-12 12:00:00')}).not.toThrow();
+    expect(function () {timespan(new Date() , new Date().toString())}).not.toThrow();
+    expect(function () {timespan(new Date() , moment())}).not.toThrow();
+  });
+
+  it('should calculate the timespan between the first two parameters as a result object with unit keys', function () {
+    var time  = new Date ();
+    var result = timespan(time, time);
+    expect(result).toEqual({ years: 0, months: 0, weeks: 0, days: 0, hours: 0, minutes: 0, seconds: 0, millis: 0 });
+
+    result = timespan('2015-01-01 12:00:00', '2016-02-09 13:01:01');
+    expect(result).toEqual({ years: 1, months: 1, weeks: 1, days: 1, hours: 1, minutes: 1, seconds: 1, millis: 0 });
+
+    result = timespan('2015-01-01 12:00:00', '2017-03-17 14:02:02');
+    expect(result).toEqual({ years: 2, months: 2, weeks: 2, days: 2, hours: 2, minutes: 2, seconds: 2, millis: 0 });
+
+    result = timespan('2014-09-20 16:44:15', '2016-03-03 01:03:10');
+    expect(result).toEqual({ years: 1, months: 5, weeks: 1, days: 4, hours: 8, minutes: 18, seconds: 55, millis: 0 });
+  });
+
+  it('should support years, months, weeks, days, hours, minutes, second and milliseconds as units', function () {
+    var result = timespan(new Date(), new Date());
+    expect(result.years  ).toBeDefined();
+    expect(result.months ).toBeDefined();
+    expect(result.weeks  ).toBeDefined();
+    expect(result.days   ).toBeDefined();
+    expect(result.hours  ).toBeDefined();
+    expect(result.minutes).toBeDefined();
+    expect(result.seconds).toBeDefined();
+    expect(result.millis ).toBeDefined();
+  });
+
+  it('should use an object of options if provided as third argument', function () {
+
+  });
+
+  it('should only set result unit keys that are selected via the options.units key', function () {
+    var options = {
+      units: {
+        months: true,
+        days: true,
+        hours: true,
+        minutes: true
+      }
+    };
+    var result = timespan('2014-09-18 16:44:15', '2015-01-13 21:49:10', options);
+    expect(result.years  ).not.toBeDefined();
+    expect(result.months ).toBeDefined();
+    expect(result.weeks  ).not.toBeDefined();
+    expect(result.days   ).toBeDefined();
+    expect(result.hours  ).toBeDefined();
+    expect(result.minutes).toBeDefined();
+    expect(result.seconds).not.toBeDefined();
+    expect(result.millis ).not.toBeDefined();
+  });
+
+  it('should accept a string containing any of "YMWDHmSs" as options.units to select result units', function () {
+    var options = {units: 'MDHm'};
+    var result = timespan('2014-09-18 16:44:15', '2015-01-13 21:49:10', options);
+    expect(result).toEqual({months: 3, days: 26, hours: 5, minutes: 4});
+
+    options = {units: 'YMD'};
+    result = timespan('1984-01-01', '2015-04-20 20:15:00', options);
+    expect(result).toEqual({years: 31, months: 3, days: 19});
+  });
+
+  it('should accept an object containing any of the supported unit keys as options.units to select result units', function () {
+    var options = {units: {months: true, days: true, hours: true, minutes: true}};
+    var result = timespan('2014-09-18 16:44:15', '2015-01-13 21:49:10', options);
+    expect(result).toEqual({months: 3, days: 26, hours: 5, minutes: 4});
+
+    options = {units: {years: true, months: true, days: true}};
+    result = timespan('1984-01-01', '2015-04-20 20:15:00', options);
+    expect(result).toEqual({years: 31, months: 3, days: 19});
+  });
+
+  it('should per default provide all requested units even if their value is 0', function () {
+    var options = {units: 'YMD'};
+    var result = timespan('2015-01-01', '2015-04-01', options);
+    expect(result).toEqual({years: 0, months: 3, days: 0});
+  });
+
+  it('should provide only those requested units in the result whos values are not 0 if options.returnZeros === false', function () {
+    var options = {units: 'YMD', returnZeros: false};
+    var result = timespan('2015-01-01', '2015-04-01', options);
+    expect(result).toEqual({months: 3});
+    expect(result.years).not.toBeDefined();
+    expect(result.days ).not.toBeDefined();
+  });
+
+  it('should provide the result to a callback if provided as options key', function (done) {
+    var options = {
+      units: 'YMD',
+      callback: function (result) {
+        expect(result).toEqual({years: 31, months: 3, days: 19});
+        done();
+      }
+    };
+    timespan('1984-01-01', '2015-04-20 20:15:00', options);
+  });
+
+  it('should return the result of the callback if callback is used', function (done) {
+    var options = {
+      units: 'YMD',
+      callback: function (result) {
+        expect(result).toEqual({years: 31, months: 3, days: 19});
+        return 'age: %Y years, %M months and %D days'
+          .replace('%Y', result.years)
+          .replace('%M', result.months)
+          .replace('%D', result.days);
+      }
+    };
+    var result = timespan('1984-01-01', '2015-04-20 20:15:00', options);
+    expect(result).toBe('age: 31 years, 3 months and 19 days');
+    done();
+  });
+
+  it('should return the result if no callack is used', function () {
+
+  });
+
+});
---
+++ timespan.js
@@ -0,0 +1,104 @@
+var moment = require('moment');
+function timespan (start, end, options) {
+  start = moment(start);
+  if (!start.isValid()) throw 'invalid start date ' + start;
+  end = moment(end);
+  if (!end.isValid()) throw 'invalid end date ' + end;
+
+  var config = {
+    units: {
+      years: true,
+      months: true,
+      weeks: true,
+      days: true,
+      hours: true,
+      minutes: true,
+      seconds: true,
+      millis: true
+    },
+    returnZeros: true,
+    callback: null
+  };
+
+  if (options instanceof Object) {
+    if (options.units instanceof String || typeof options.units === 'string') {
+      if (options.units.search('Y') === -1) config.units.years = false;
+      if (options.units.search('M') === -1) config.units.months = false;
+      if (options.units.search('W') === -1) config.units.weeks = false;
+      if (options.units.search('D') === -1) config.units.days = false;
+      if (options.units.search('H') === -1) config.units.hours = false;
+      if (options.units.search('m') === -1) config.units.minutes = false;
+      if (options.units.search('S') === -1) config.units.seconds = false;
+      if (options.units.search('s') === -1) config.units.millis = false;
+    } else if (typeof options.units == 'object') {
+      if (!options.units.years  ) config.units.years   = false;
+      if (!options.units.months ) config.units.months  = false;
+      if (!options.units.weeks  ) config.units.weeks   = false;
+      if (!options.units.days   ) config.units.days    = false;
+      if (!options.units.hours  ) config.units.hours   = false;
+      if (!options.units.minutes) config.units.minutes = false;
+      if (!options.units.seconds) config.units.seconds = false;
+      if (!options.units.millis ) config.units.millis  = false;
+    }
+
+    if (options.returnZeros === false) config.returnZeros = false;
+    if (typeof options.callback === 'function') config.callback = options.callback;
+  }
+
+  var result = {};
+
+  if (config.units.years) {
+    var years = end.diff(start, 'years');
+    start.add(years, 'years');
+    if (config.returnZeros || years != 0) result.years = years;
+  }
+
+  if (config.units.months) {
+    var months = end.diff(start, 'months');
+    start.add(months, 'months');
+    if (config.returnZeros || months != 0) result.months = months;
+  }
+
+  if (config.units.weeks) {
+    var weeks = end.diff(start, 'weeks');
+    start.add(weeks, 'weeks');
+    if (config.returnZeros || weeks != 0) result.weeks = weeks;
+  }
+
+  if (config.units.days) {
+    var days = end.diff(start, 'days');
+    start.add(days, 'days');
+    if (config.returnZeros || days != 0) result.days = days;
+  }
+
+  if (config.units.hours) {
+    var hours = end.diff(start, 'hours');
+    start.add(hours, 'hours');
+    if (config.returnZeros || hours != 0) result.hours = hours;
+  }
+
+  if (config.units.minutes) {
+    var minutes = end.diff(start, 'minutes');
+    start.add(minutes, 'minutes');
+    if (config.returnZeros || minutes != 0) result.minutes = minutes;
+  }
+
+  if (config.units.seconds) {
+    var seconds = end.diff(start, 'seconds');
+    start.add(seconds, 'seconds');
+    if (config.returnZeros || seconds != 0) result.seconds = seconds;
+  }
+
+  if (config.units.millis) {
+    var millis = end.valueOf() - start.valueOf();
+    if (config.returnZeros || millis != 0) result.millis = millis;
+  }
+
+  if (config.callback) {
+    return config.callback(result);
+  }
+
+  return result;
+}
+
+module.exports = timespan;
